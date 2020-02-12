e3eb7723accabfed20249f781821902c81cbc545
araki2410 <araki2410@gmail.com>
2020-02-12 21:02:19 +0900

    Fix: sighalf

--- RGB/architecture.py
+++ RGB/architecture.py
@@ -161,12 +161,12 @@ class RNN(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
         tmp3 = sig(x)
-        x = self.layer1(tmp1*sig(x))
+        x = self.layer1(tmp1*x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
         tmp4 = sig(x)
-        x = self.avgpool(tmp2*sig(x))
+        x = self.avgpool(tmp2*x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
 
@@ -177,12 +177,12 @@ class RNN(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
 
-        x = self.layer1(tmp3*sig(x))
+        x = self.layer1(tmp3*x)
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
 
-        x = self.avgpool(tmp4*sig(x))
+        x = self.avgpool(tmp4*x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
 
